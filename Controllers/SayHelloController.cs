f4c27dd043ad853371011c3cc27559f42f1dec05
MandoxaElemental <gprcards@gmail.com>
2025-01-06 14:53:24 -0800

    first commit

---
+++ Controllers/SayHelloController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Say_Hello_Endpoint.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SayHelloController : ControllerBase
+    {
+         public List<string> nameList = new();
+        [HttpPost]
+        [Route("AddName/{nameToAdd}")]
+        public List<string> AddNameToList(string nameToAdd){
+            nameList.Add(nameToAdd);
+            return 
+        }
+    }
+}
